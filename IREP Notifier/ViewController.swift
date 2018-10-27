50c1e7d9b779ad10fbe8f64111a55328e8c73e82
Chin Kerk <kerkchinwee@hotmail.co.uk>
2018-10-27 16:58:11 +0800

    Initial Commit

---
+++ IREP Notifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IREP Notifier
+//
+//  Created by Chin Wee Kerk on 27/10/2018.
+//  Copyright © 2018 Chin Wee Kerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
