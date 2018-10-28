9ca68c72feb1f635d8c6c35aa518a7ac549974d5
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2018-10-28 22:27:26 +0800

    Update project

---
+++ IREP Notifier/Models/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  IREP Notifier
+//
+//  Created by Aaron Lee on 28/10/18.
+//  Copyright © 2018 Chin Wee Kerk. All rights reserved.
+//
+
+import RxSwift
+
+struct LoginViewModel {
+  
+}
