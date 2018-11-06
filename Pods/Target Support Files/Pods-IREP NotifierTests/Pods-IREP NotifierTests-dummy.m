01578eb1bfe0acd2cc1499c4a48721bb45de5678
kerkcw <kerkcw@m1.com.sg>
2018-11-06 11:19:04 +0800

    Remove Pods dependencies

--- Pods/Target Support Files/Pods-IREP NotifierTests/Pods-IREP NotifierTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IREP_NotifierTests : NSObject
-@end
-@implementation PodsDummy_Pods_IREP_NotifierTests
-@end
--- Pods/Target Support Files/Pods-IREP NotifierUITests/Pods-IREP NotifierUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IREP_NotifierUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IREP_NotifierUITests
-@end
