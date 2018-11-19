af34a8e8ae87b581dd4a0c139c8fd2b3b4287ffc
kerkcw <kerkcw@m1.com.sg>
2018-11-19 16:49:10 +0800

    Update project

--- IREP Notifier/Views/ViewControllers/AccountLoginViewController.swift
+++ IREP Notifier/Views/ViewControllers/AccountLoginViewController.swift
@@ -16,4 +16,8 @@ class AccountLoginViewController: UIViewController {
   @IBOutlet weak var usernameTextField: UITextField!
   @IBOutlet weak var passwordTextField: UITextField!
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
 }
